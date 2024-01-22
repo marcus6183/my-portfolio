9f34d6c9f8a37cca698e84f4255beb7e8e9a5e89
marcus6183 <marcusw6183@gmail.com>
2024-01-22 21:50:30 +0800

    Completed portfolio component
    Removed light-up hover effect
    Projects are stored in an array where each project is an object with the following properties:
    1. thumbnailPath: string
    2. projectName: string
    3. githubURL: string
    4. techStack: array
    
    The projects are dynamically rendered

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,16 +1,34 @@
-import testingTN from "/carrot-keebs-thumbnail.png";
 import PortfolioCard from "./PortfolioCard";
 
 const Portfolio = () => {
+    const projects = [
+        {
+            thumbnailPath: "/carrot-keebs-thumbnail.png",
+            projectName: "Carrotkeebs",
+            githubURL:
+                "https://github.com/marcus6183/COS30043-Custom-Web-Application",
+            techStack: ["Vue", "Bootstrap", "Firebase"],
+        },
+        {
+            thumbnailPath: "/KPI-Management-System.png",
+            projectName: "KPI-System",
+            githubURL: "https://github.com/marcus6183/KPI-Management-System",
+            techStack: ["PHP", "MySQL"],
+        },
+    ];
     return (
-        <section name="portfolio" className="h-screen w-full bg-neutral-900">
+        <section
+            name="portfolio"
+            className="min-h-screen w-full bg-neutral-900 pt-12"
+        >
             <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-6">
                 <h2 className="text-6xl font-bold py-6 text-transparent bg-clip-text bg-gradient-to-br from-white via-stone-300 to-stone-500 inline-block">
                     Portfolio
                 </h2>
-                <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-8 px-12 sm:px-0">
-                    <PortfolioCard />
-                    <PortfolioCard />
+                <div className="grid lg:grid-cols-2 gap-8 px-12 sm:px-0 mb-10">
+                    {projects.map((project, index) => (
+                        <PortfolioCard key={index} project={project} />
+                    ))}
                 </div>
             </div>
         </section>
--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,27 +1,42 @@
 import styles from "./PortfolioCard.module.css";
-import { useState } from "react";
+import { FaGithub } from "react-icons/fa";
 
-const PortfolioCard = () => {
-    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
-    const handleMouseMove = (event) => {
-        const rect = event.currentTarget.getBoundingClientRect();
-        const x = event.clientX - rect.left;
-        const y = event.clientY - rect.top;
-
-        setMousePosition({ x, y });
-    };
-
+const PortfolioCard = (props) => {
+    const { project } = props;
     return (
-        <div
-            className={styles.card}
-            onMouseMove={handleMouseMove}
-            style={{
-                "--mouse-x": `${mousePosition.x}px`,
-                "--mouse-y": `${mousePosition.y}px`,
-            }}
-        >
-            Hello World
+        <div className={styles.card}>
+            {/* Inner div for CSS Tailwind styling */}
+            <div className="w-full h-full pt-4 rounded-xl flex justify-center flex-wrap group">
+                {/* Thumbnail Container */}
+                <div className="w-full h-56 rounded-t-xl flex items-end justify-center overflow-hidden">
+                    <div className="w-[22rem] h-52 overflow-hidden rounded-t-lg group-hover:scale-105 group-hover:-rotate-1 duration-200 ease-in-out">
+                        <img
+                            className="w-full h-full object-cover"
+                            src={project.thumbnailPath}
+                            alt=""
+                        />
+                    </div>
+                </div>
+                <div className="w-full px-12 -mt-4 flex items-center gap-5">
+                    <h3 className="text-xl font-bold whitespace-nowrap text-neutral-500 bg-clip-text inline-block bg-gradient-to-br from-white via-stone-300 to-stone-500 group-hover:animate-gradientText group-hover:text-transparent duration-200">
+                        {project.projectName}
+                    </h3>
+                    <div className="w-full h-[0.5px] bg-neutral-400 group-hover:bg-gradient-to-r from-emerald-100 via-cyan-400 to-blue-500 duration-200"></div>
+                    <a
+                        href={project.githubURL}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <FaGithub
+                            className="text-neutral-500 hover:text-white duration-200"
+                            size={25}
+                        />
+                    </a>
+                </div>
+                <div className="w-full px-12 text-neutral-600 group-hover:text-neutral-500 duration-200">
+                    <p>{project.techStack.join(" - ")}</p>
+                </div>
+            </div>
         </div>
     );
 };
